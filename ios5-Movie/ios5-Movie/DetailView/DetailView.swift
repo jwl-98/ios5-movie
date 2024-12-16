eb02ee9808f1be2fdaef8b8debc1c8e25a6f7add
youngjin <bryjna07@nate.com>
2024-12-16 16:03:14 +0900

    feat: DetailView 기본 UI 구현

--- ios5-Movie/ios5-Movie/DetailView/DetailView.swift
+++ ios5-Movie/ios5-Movie/DetailView/DetailView.swift
@@ -12,18 +12,52 @@ import SwiftUI
 
 final class DetailView: UIView {
     
-    
-    private let detailImageView: UIImageView = {
-        let image = UIImageView()
-        image.backgroundColor = .gray
-        return image
+    lazy var detailImageView: UIImageView = {
+        let imageView = UIImageView()
+        // imageView.backgroundColor = .gray
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleToFill
+        imageView.layer.cornerRadius = 20
+        return imageView
     }()
-    
-    
-    private lazy var imageContainView: UIView = {
-        let view = UIView()
-        view.addSubview(detailImageView)
-        return view
+    let movieNameLable: UILabel = {
+        let label = UILabel()
+        label.text = "Test"
+        label.font = .boldSystemFont(ofSize: 16)
+        return label
+    }()
+    let voteAverageLable: UILabel = {
+        let label = UILabel()
+        label.text = "0.00"
+        label.font = .systemFont(ofSize: 12)
+        return label
+    }()
+    private let movieIntroduceLable: UILabel = {
+        let label = UILabel()
+        label.text = "영화소개"
+        label.font = .boldSystemFont(ofSize: 16)
+        return label
+    }()
+    let movieDescriptionLable: UILabel = {
+        let label = UILabel()
+        label.text = "죽음의 문턱에서 맹수의 초인적인 힘을 얻고 살아 돌아온 크레이븐이 무자비한 복수의 길을 택하며 거침없는 사냥을 펼치는 액션 블록버스터"
+        label.numberOfLines = .max
+        label.font = .systemFont(ofSize: 14)
+        return label
+    }()
+    let releaseDateLable: UILabel = {
+        let label = UILabel()
+        label.text = "출시일"
+        label.font = .systemFont(ofSize: 14)
+        return label
+    }()
+    lazy var reservationButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("예매하기", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.titleLabel?.font = .systemFont(ofSize: 14)
+        button.layer.cornerRadius = 15
+        return button
     }()
     
     //MARK: - 생성자 셋팅
@@ -39,33 +73,61 @@ final class DetailView: UIView {
     
     // MARK: - 오토레이아웃
     private func configureUI() {
-        self.addSubview(imageContainView)
-        
-        imageContainView.snp.makeConstraints { make in
-            make.top.equalToSuperview().inset(10)
-            make.centerX.equalToSuperview()
-            make.height.width.equalTo(400)
-        }
+        [
+            detailImageView,
+            movieNameLable,
+            voteAverageLable,
+            movieIntroduceLable,
+            movieDescriptionLable,
+            releaseDateLable,
+            reservationButton
+        ].forEach { self.addSubview($0) }
         
         detailImageView.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-            make.height.equalTo(335)
-            make.width.equalTo(238)
-            
+            make.top.equalToSuperview().inset(30)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(300)
+            make.width.equalTo(240)
+        }
+        movieNameLable.snp.makeConstraints { make in
+            make.leading.equalTo(detailImageView.snp.leading).offset(5)
+            make.top.equalTo(detailImageView.snp.bottom).offset(30)
+        }
+        voteAverageLable.snp.makeConstraints { make in
+            make.leading.equalTo(detailImageView.snp.leading).offset(5)
+            make.top.equalTo(movieNameLable.snp.bottom).offset(5)
+        }
+        movieIntroduceLable.snp.makeConstraints { make in
+            make.leading.equalTo(detailImageView.snp.leading).offset(5)
+            make.top.equalTo(voteAverageLable.snp.bottom).offset(30)
+        }
+        movieDescriptionLable.snp.makeConstraints { make in
+            make.top.equalTo(movieIntroduceLable.snp.bottom).offset(10)
+            make.leading.equalTo(detailImageView.snp.leading).offset(5)
+            make.trailing.equalTo(detailImageView.snp.trailing).offset(-5)
+        }
+        releaseDateLable.snp.makeConstraints { make in
+            make.leading.equalTo(detailImageView.snp.leading).offset(5)
+            make.top.equalTo(movieDescriptionLable.snp.bottom).offset(30)
+        }
+        reservationButton.snp.makeConstraints { make in
+            make.top.equalTo(releaseDateLable.snp.bottom).offset(20)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(90)
         }
     }
-    
 }
+
 // MARK: - SwiftUI Preview Extension
 extension DetailViewController {
     private struct Preview: UIViewControllerRepresentable {
         func makeUIViewController(context: Context) -> UIViewController {
             return DetailViewController()
         }
-
+        
         func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
     }
-
+    
     func toPreview() -> some View {
         Preview()
     }
--- ios5-Movie/ios5-Movie/DetailView/DetailViewController.swift
+++ ios5-Movie/ios5-Movie/DetailView/DetailViewController.swift
@@ -12,17 +12,26 @@ final class DetailViewController: UIViewController {
     
     private let detailView = DetailView()
     
+    private let dataManager = DummyMovieDataManager()
+    
+    private var movie: [DummyMovie] = []
+    
     private let networkManager = NetworkManager.shared
     
     override func loadView() {
         self.view = detailView
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        dataManager.makeMovieData()
+        setupDatas()
     }
 
+    private func setupDatas() {
+        // 셀 선택된 데이터 넘기기
+        movie = dataManager.getDummyMovieData()
+        detailView.detailImageView.image = movie[0].movieImage
+        detailView.movieNameLable.text = movie[0].movieName
+    }
 }
-
-
